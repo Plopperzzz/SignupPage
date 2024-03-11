05437d2644567261cca2fc59740e14b4868c75e1
Christian Anderson <christian.anderson982@gmail.com>
2024-03-10 21:30:31 -0400

    added password matching evaluation and corresponding classes to indicate a mis-match

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
-const btn = document.querySelector("btn");
+console.log("HELKL");
+const btn = document.querySelector('button[type="submit"]');
+console.log(btn);
 
 btn.addEventListener('click', e => {
     const pass = document.querySelector('input[name="user_password"]');
     const confirm = document.querySelector('input[name="user_conpassword"]');
 
-    console.log(pass);
-    console.log(confirm);
+    if(pass !== confirm)
+    {
+        pass.classList.remove('input');
+        pass.classList.add('error');
+        return;
+    }
 })
+
+function checkpass(){
+    const pass = document.querySelector('input[name="user_password"]');
+    const confirm = document.querySelector('input[name="user_conpassword"]');
+    const errorPhrase = document.querySelector('.pass-error');
+
+    if(pass.value !== confirm.value)
+    {
+        confirm.classList.add('error');
+        errorPhrase.textContent = "Passwords do not match";
+    }
+    else{
+        confirm.classList.remove('error');
+        errorPhrase.textContent = "";
+    }
+}
